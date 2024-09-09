a5c661fea66a767c9975dc9a1cceff0fc3afd563
seansteel3 <sean.steele.a@gmail.com>
2024-09-09 17:11:10 -0400

    added tblastn database search and changed blastn query from input to the temp file output of tblastn

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-blastn -query $1 -subject $2  -perc_identity 30 -qcov_hsp_perc 90 -outfmt 6  -out $3
+tblastn -query $1 -db nt -out temp_file.txt
+blastn -query temp_file.txt -subject $2  -perc_identity 30 -qcov_hsp_perc 90 -outfmt 6  -out $3
 number_match=$(grep -c '^' $3)
 number_imperfect=$(awk '$5 != 0 {count++} END {print count}' $3)
 final_perfect=$((number_match - number_imperfect))
