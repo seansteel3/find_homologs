ae205cebb9f82840221ad51eb918566418a400dc
seansteel3 <sean.steele.a@gmail.com>
2024-09-09 18:56:25 -0400

    suppressed the making database prints

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-makeblastdb -in $2 -dbtype nucl -out ./db/temp_db
+makeblastdb -in $2 -dbtype nucl -out ./db/temp_db > /dev/null
 tblastn -query $1 -db ./db/temp_db -out temp_file.txt -outfmt 6
 awk '$3 > 30 && $4 > 90' temp_file.txt > $3
 rm -r ./db
 rm temp_file.txt
-echo wc -l $3
+wc -l $3
