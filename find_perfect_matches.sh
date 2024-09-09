bc2291338967203741dd4ffc9440b6b16ab95b52
seansteel3 <sean.steele.a@gmail.com>
2024-09-09 15:31:44 -0400

    inital perfect hits file commit

---
+++ find_perfect_matches.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+blastn -query $1 -subject $2  -perc_identity 100 -ungapped -outfmt 6  -out $3
+number_match=$(grep -c '^' $3)
+number_imperfect=$(awk '$5 != 0 {count++} END {print count}' $3)
+final_perfect=$((number_match - number_imperfect))
+echo $final_perfect
