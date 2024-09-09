00a824a2c9faa34888a4ba8c973b8340c7345caa
seansteel3 <sean.steele.a@gmail.com>
2024-09-09 18:50:37 -0400

    added count of matches display

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 makeblastdb -in $2 -dbtype nucl -out ./db/temp_db
 tblastn -query $1 -db ./db/temp_db -out temp_file.txt -outfmt 6
-#awk '$3 > 30 && $4 > 90' temp_file.txt > $3
+awk '$3 > 30 && $4 > 90' temp_file.txt > $3
 rm -r ./db
 rm temp_file.txt
+echo wc -l $3
