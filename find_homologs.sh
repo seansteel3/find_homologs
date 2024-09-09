caf5bee705e0d01b036a6dce327014391b4fe4f1
seansteel3 <sean.steele.a@gmail.com>
2024-09-09 17:08:03 -0400

    added qcov_hsp_perc 90 to ensure greater than 90% minimum length

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-blastn -query $1 -subject $2  -perc_identity 30 -outfmt 6  -out $3
+blastn -query $1 -subject $2  -perc_identity 30 -qcov_hsp_perc 90 -outfmt 6  -out $3
 number_match=$(grep -c '^' $3)
 number_imperfect=$(awk '$5 != 0 {count++} END {print count}' $3)
 final_perfect=$((number_match - number_imperfect))
